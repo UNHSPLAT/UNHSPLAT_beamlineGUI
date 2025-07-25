772899cad358c7ac499f4fdf4323cc0c7b4e5039
UNHSplat <unhsplat@gmail.com>
2025-07-25 15:51:43 -0400

    bug fix and added beep warning

--- ValveControlScript.lua
+++ ValveControlScript.lua
@@ -7,7 +7,7 @@ function valveSafety()
     local beamRoughV1
     local chamberBeamGV
     local beamRoughV2
-    local chamberRoughv2
+    local chamberRoughV2
     local chamberRoughV1
     local cryo1GV
 
@@ -17,21 +17,25 @@ function valveSafety()
     turbo1GV = 4
     cryo1GV = 5
     chamberBeamGV = 6
-    chamberRoughv2 = 7
+    chamberRoughV2 = 7
     chamberRoughV1 = 8
 
     function turbo1GV_safeOpen()
         --Checks that beamRoughV1 is on and beamRoughV2 is closed before opening turbo1GV
-        local safe
-        safe = true
+
         if outlet[turbo1GV].physical_state == on or outlet[turbo1GV].transient_state == on then 
             if outlet[beamRoughV1].physical_state == off then
             --outlet beamRoughV1 is off, not safe to proceed    
-                LOG("outlet beamRoughV1 is off, enable rough pump to open valve")
                 outlet[turbo1GV].off()
+                BEEP(ON)
+				LOG("outlet beamRoughV1 is off, enable rough pump to open valve")
+				BEEP(OFF)
             elseif outlet[beamRoughV2].physical_state == on then
-                LOG("beamRoughV2 must be closed to open turbo1GV")
                 outlet[turbo1GV].off()
+                BEEP(ON)
+				LOG("beamRoughV2 must be closed to open turbo1GV")
+				BEEP(OFF)
+                
             end
         end
     end
@@ -43,11 +47,17 @@ function valveSafety()
         if outlet[turbo2GV].physical_state == on or outlet[turbo2GV].transient_state == on then 
             if outlet[beamRoughV1].physical_state == off then
             --outlet beamRoughV1 is off, not safe to proceed    
-                LOG("outlet beamRoughV1 is off, enable rough pump to open valve")
                 outlet[turbo2GV].off()
+                BEEP(ON)
+				LOG("outlet beamRoughV1 is off, enable rough pump to open valve")
+				BEEP(OFF)
+                
             elseif outlet[beamRoughV2].physical_state == on then
-                LOG("beamRoughV2 must be closed to open turbo2GV")
                 outlet[turbo2GV].off()
+                BEEP(ON)
+				LOG("beamRoughV2 must be closed to open turbo2GV")
+				BEEP(OFF)
+                
             end
         end
     end
@@ -55,28 +65,43 @@ function valveSafety()
     function cryo1GV_safeOpen()
         --Checks that chamberRoughV2 is closed AND either chamberBeamGV or beamRoughV2 is closed
         if outlet[cryo1GV].physical_state == on or outlet[cryo1GV].transient_state == on then
-            if outlet[chamberRoughv2].physical_state == on then
-                LOG("chamberRoughV2 must be closed to open cryo1GV")
+            if outlet[chamberRoughV2].physical_state == on then
                 outlet[cryo1GV].off()
+                BEEP(ON)
+				LOG("chamberRoughV2 must be closed to open cryo1GV")
+				BEEP(OFF)
+                
             elseif outlet[chamberBeamGV].physical_state == on and outlet[beamRoughV2].physical_state == on then
-                LOG("either chamberBeamGV or beamRoughV2 must be closed to open cryo1GV")
                 outlet[cryo1GV].off()
+                BEEP(ON)
+				LOG("either chamberBeamGV or beamRoughV2 must be closed to open cryo1GV")
+				BEEP(OFF)
+                
             end
         end
     end
 
     function chamberRoughV2_safeOpen()
         --Checks that cryo1GV is closed, chamberRoughV2 is closed and chamberRoughV1 is open
-        if outlet[chamberRoughv2].physical_state == on or outlet[chamberRoughv2].transient_state == on then
+        if outlet[chamberRoughV2].physical_state == on or outlet[chamberRoughV2].transient_state == on then
             if outlet[cryo1GV].physical_state == on then
-                LOG("cryo1GV must be closed to open chamberRoughV2")
-                outlet[chamberRoughv2].off()
-            elseif outlet[beamRoughv2].physical_state == on then
-                LOG("chamberRoughV2 must be closed to proceed")
-                outlet[chamberRoughv2].off()
+                outlet[chamberRoughV2].off()
+                BEEP(ON)
+				LOG("cryo1GV must be closed to open chamberRoughV2")
+				BEEP(OFF)
+               
+            elseif outlet[beamRoughV2].physical_state == on then
+                outlet[chamberRoughV2].off()
+                BEEP(ON)
+				LOG("chamberRoughV2 must be closed to proceed")
+				BEEP(OFF)
+                
             elseif outlet[chamberRoughV1].physical_state == off then
-                LOG("chamberRoughV1 must be open to open chamberRoughV2")
-                outlet[chamberRoughv2].off()
+                outlet[chamberRoughV2].off()
+                BEEP(ON)
+				LOG("chamberRoughV1 must be open to open chamberRoughV2")
+				BEEP(OFF)
+                
             end
         end
     end
@@ -85,20 +110,35 @@ function valveSafety()
         --Checks that turbo1GV, turbo2GV, chamberRoughV2, and chamberBeamGV are closed, and chamberRoughV1 is open
         if outlet[beamRoughV2].physical_state == on or outlet[beamRoughV2].transient_state == on then
             if outlet[turbo1GV].physical_state == on then
-                LOG("turbo1GV must be closed to open beamRoughV2")
                 outlet[beamRoughV2].off()
+                BEEP(ON)
+				LOG("turbo1GV must be closed to open beamRoughV2")
+				BEEP(OFF)
+                
             elseif outlet[turbo2GV].physical_state == on then
-                LOG("turbo2GV must be closed to open beamRoughV2")
                 outlet[beamRoughV2].off()
-            elseif outlet[chamberRoughv2].physical_state == on then
-                LOG("chamberRoughV2 must be closed to open beamRoughV2")
+                BEEP(ON)
+				LOG("turbo2GV must be closed to open beamRoughV2")
+				BEEP(OFF)
+                
+            elseif outlet[chamberRoughV2].physical_state == on then
                 outlet[beamRoughV2].off()
+                BEEP(ON)
+				LOG("chamberRoughV2 must be closed to open beamRoughV2")
+				BEEP(OFF)
+               
             elseif outlet[chamberBeamGV].physical_state == on then
-                LOG("chamberBeamGV must be closed to open beamRoughV2")
                 outlet[beamRoughV2].off()
+                BEEP(ON)
+				LOG("chamberBeamGV must be closed to open beamRoughV2")
+				BEEP(OFF)
+                
             elseif outlet[chamberRoughV1].physical_state == off then
-                LOG("chamberRoughV1 must be open to open beamRoughV2")
                 outlet[beamRoughV2].off()
+                BEEP(ON)
+				LOG("chamberRoughV1 must be open to open beamRoughV2")
+				BEEP(OFF)
+                
             end
         end
     end
@@ -113,8 +153,15 @@ function valveSafety()
                                 outlet[cryo1GV].physical_state == off
 
             if not ((anyBeamGVOpen and chamberOpen) or allGVsClosed) then
-                LOG("Either one beam GV and chamber GV must be open, or all GVs must be closed")
                 outlet[chamberBeamGV].off()
+                BEEP(ON)
+				LOG("Either one beam GV and chamber GV must be open, or all GVs must be closed")
+				BEEP(OFF)
+			elseif outlet[chamberRoughV2].physical_state == on then
+                outlet[chamberBeamGV].off()
+                BEEP(ON)
+				LOG("Must close chamberRoughV2")
+				BEEP(OFF)
             end
         end
     end
