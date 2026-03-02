269ce2ab342032b702d78047f8e8b09fcb66bece
jonbowr <jonbowr@yahoo.com>
2026-03-02 10:57:36 -0500

    added the picoammeter for PDH current detection

--- labGUI/SWIPS_GUI/setupSWIPSInstruments.m
+++ labGUI/SWIPS_GUI/setupSWIPSInstruments.m
@@ -2,12 +2,47 @@ function instruments = setupSWIPSInstruments()
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+    % Configure picoammeter
+    function config_picoFaraday(hFaraday)
+        trynum = 3;
+        if hFaraday.Connected
+%             hFaraday.Tag = "Faraday";
+            hFaraday.devRW(':SYST:ZCH OFF');
+            dataOut = strtrim(hFaraday.devRW(':SYST:ZCH?'));
+            i = 1;
+            while ~strcmp(dataOut,'0') && trynum<i
+                warning('beamlineGUI:keithleyNonresponsive','Keithley not listening! Zcheck did not shut off as expected...');
+                hFaraday.devRW(':SYST:ZCH OFF');
+                dataOut = strtrim(hFaraday.devRW(':SYST:ZCH?'));
+                trynum=trynum+1;
+            end
+            hFaraday.devRW('ARM:COUN 1');
+            dataOut = strtrim(hFaraday.devRW('ARM:COUN?'));
+            i = 1;
+            while ~strcmp(dataOut,'1') && trynum<i
+                warning('beamlineGUI:keithleyNonresponsive','Keithley not listening! Arm count did not set to 1 as expected...');
+                hFaraday.devRW('ARM:COUN 1');
+                dataOut = strtrim(hFaraday.devRW('ARM:COUN?'));
+                trynum=trynum+1;
+            end
+            hFaraday.devRW('FORM:ELEM READ');
+            dataOut = strtrim(hFaraday.devRW('FORM:ELEM?'));
+            i = 1;
+            while ~strcmp(dataOut,'READ') && trynum<i
+                warning('beamlineGUI:keithleyNonresponsive','Keithley not listening! Output format not set to ''READ'' as expected...');
+                hFaraday.devRW('FORM:ELEM READ');
+                dataOut = strtrim(hFaraday.devRW('FORM:ELEM?'));
+                trynum=trynum+1;
+            end
+            hFaraday.devRW(':SYST:LOC');
+        end
+    end
   
      instruments = struct('Opal_Kelly',SWIPS_OK(),...
                                     'caen_HVPS1',caen_hvps([],[],0,'config_caenPS.ini'),...
                                     "HvMCPn",srsPS350('GPIB0::04::INSTR'),...
                                     'newportStage',NewportStageControl('192.168.0.254'),...
-                         "flukeHydra",flukeHydra2620A("GPIB0::6::INSTR")...
+                         "picoPHD",keithley6485('GPIB0::14::INSTR',@config_picoFaraday)...
                                 );
 
      % Configure the Stanford research read functions
@@ -19,6 +54,12 @@ function instruments = setupSWIPSInstruments()
          end
      end
      instruments.HvMCPn.readFunc = @read_srsHVPS;
+
+     % Configure the picoammeter read function
+     function val = read_pico(self)
+            val  = self.readDev();
+     end
+     instruments.picoFaraday.readFunc = @read_pico;
 
      % connect to devices
     instruments.caen_HVPS1.connectDevice();
